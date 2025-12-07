4b40f2be5b7f4fbfdb89ad945955dfd6f0a1b597
AVJoshua <akalihujoshua@gmail.com>
2025-12-07 17:10:26 +0100

    Completed random quote generator UI

--- randomQuoteGenerator/randomQuoteGenerator.js
+++ randomQuoteGenerator/randomQuoteGenerator.js
@@ -1,6 +1,35 @@
+const colors = [
+    ["#FF8080", "#3d2202ff"],
+    ["#FFF3DA", "#2f55d1ff"],
+    ["#1b6653ff", "#82A0D8"],
+    ["#28190cff", "#90de3eff"],
+    ["#e7eff6ff", "#7e4b0bff"],
+    ["#b7b7b7ff", "#522005ff"],
+    ["#FFDEB4", "#953c3cff"],
+    ["#FFAACF", "#B9F3E4"],
+    ["#173221ff", "#4e792bff"],
+    ["#F7ECDE", "#54BAB9"],
+    ["#7897AB", "#2f111cff"],
+    ["#B97A95", "#1299ceff"]
+]
+
+const apiKey = "jDrPRvhBbu66u9OZb1vHEQ==HdzosyIOBP267OqK"
+
+function randomNumber(array) {
+    const randomNumber = Math.floor(Math.random()  * array.length + 1)
+    return array[randomNumber]
+}
+
+
+
 function getNewRandomQuote () {
     // Make a GET request to the Quotable API
-fetch("https://api.quotable.io/quotes/random")
+fetch("https://api.api-ninjas.com/v1/quotes", {
+    method: "GET", 
+    headers: {
+        "X-Api-Key": apiKey
+    }
+})
   .then(response => {
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
@@ -8,7 +37,9 @@ fetch("https://api.quotable.io/quotes/random")
     return response.json(); // Parse JSON response
   })
   .then(data => {
-    const quoteText = data[0].content
+    let colorCombo = randomNumber(colors)
+    document.getElementById("random-quote-generator").style.background = `linear-gradient(45deg, ${colorCombo[0]}, ${colorCombo[1]})`
+    const quoteText = data[0].quote
     const quoteAuthor = data[0].author
     document.getElementById("random-quote-text").innerHTML = quoteText
     document.getElementById("random-quote-author").innerHTML = quoteAuthor
